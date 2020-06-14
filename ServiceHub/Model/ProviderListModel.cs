1d39e040d3ffeca989cb6224c2551371fed708c1
Lavrenti DeLavrenti <one.lasha@gmail.com>
2020-06-13 21:43:34 -0700

    ver: 1.88 Provider List

---
+++ ServiceHub/Model/ProviderListModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceHub.Model
+{
+    public class ProviderListModel
+    {
+        public int RowNum { get; set; }
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string ProviderType { get; set; }
+        public string Comment { get; set; }
+        public string TaxNo { get; set; }
+        public string BankName { get; set; }
+        public string AccountNo { get; set; }
+        public string ContactPerson { get; set; }
+        public string Phone { get; set; }
+        public string Region { get; set; }
+        public string Raion { get; set; }
+        public string Address { get; set; }
+        public string Manager { get; set; }
+        public string Status { get; set; }
+        public int clrfg { get; set; }
+    }
+}
