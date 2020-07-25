6f7e1e0725b5afa5c6a6ea99d9668ba07891d3c4
Lavrenti DeLavrenti <one.lasha@gmail.com>
2020-07-24 18:31:54 -0700

    Adding Loging Controller Tests

--- ServiceHubTests/LoginControllerTest.cs
+++ ServiceHubTests/LoginControllerTest.cs
@@ -4,10 +4,15 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace ServiceHubTests
 {
     [TestClass]
-    public class LoginTest
+    public class LoginControllerTest
     {
         [TestMethod]
         public void Login_TestUser_Ok()
+        {
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void Login_AdminUser_Ok()
         {
             Assert.IsTrue(true);
         }
