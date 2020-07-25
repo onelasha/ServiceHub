645a79cca8bdfad20c285875e34443437090d030
Lavrenti DeLavrenti <one.lasha@gmail.com>
2020-07-24 17:38:04 -0700

    Adding Tests

---
+++ ServiceHubTests/LoginTest.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace ServiceHubTests
+{
+    public class LoginTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Login_TestUser_Ok()
+        {
+            //Assert.Pass();
+            Assert.IsTrue(true);
+        }
+    }
+}
